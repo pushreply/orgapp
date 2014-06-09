066af1725030508e40c4a6396be10fd823dc3a69
oliverneubauer77@gmail.com <oliverneubauer77@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-06-09 12:39:27 +0000

    Add the menu items to show "private information", "security information" and "event_history"

--- src/fhkl/de/orgapp/util/MenuActivity.java
+++ src/fhkl/de/orgapp/util/MenuActivity.java
@@ -9,59 +9,87 @@ import android.view.MenuItem;
 import com.example.orgapp.R;
 
 import fhkl.de.orgapp.controller.CalendarController;
+import fhkl.de.orgapp.controller.EventHistoryController;
 import fhkl.de.orgapp.controller.NotificationController;
+import fhkl.de.orgapp.controller.PrivateInfoController;
 import fhkl.de.orgapp.controller.ProfilController;
+import fhkl.de.orgapp.controller.SecurityInfoController;
 
-public class MenuActivity extends Activity {
-
+public class MenuActivity extends Activity
+{
 	private String personIdLoggedPerson;
 
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main_menu, menu);
-		if (getIntent().getComponent().getClassName()
-				.equals(NotificationController.class.getName())) {
+		
+		if(getIntent().getComponent().getClassName().equals(NotificationController.class.getName()))
+		{
 			menu.findItem(R.id.NOTIFICATION_SETTINGS).setVisible(true);
+		}
+		
+		if(getIntent().getComponent().getClassName().equals(ProfilController.class.getName()))
+		{
+			menu.findItem(R.id.PROFIL_SETTINGS).setVisible(true);
 		}
 
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
 		Intent intent;
 		personIdLoggedPerson = getIntent().getStringExtra("UserId");
 
-		switch (item.getItemId()) {
-
-		case R.id.CALENDAR:
-			intent = new Intent(MenuActivity.this, CalendarController.class);
-			intent.putExtra("UserId", personIdLoggedPerson);
-			startActivity(intent);
-			return true;
-
-		case R.id.NOTIFICATIONS:
-			intent = new Intent(MenuActivity.this, NotificationController.class);
-			intent.putExtra("UserId", personIdLoggedPerson);
-			startActivity(intent);
-			return true;
-
-		case R.id.PROFIL:
-			intent = new Intent(MenuActivity.this, ProfilController.class);
-			intent.putExtra("UserId", personIdLoggedPerson);
-			startActivity(intent);
-			return true;
-
-		case R.id.NOTIFICATION_SETTINGS:
-			intent = new Intent(MenuActivity.this, NotificationController.class);
-			intent.putExtra("UserId", personIdLoggedPerson);
-			intent.putExtra("Notification_Settings", "true");
-			startActivity(intent);
-			return true;
-
-		default:
-			return false;
-
+		switch(item.getItemId())
+		{
+			case R.id.CALENDAR:
+				intent = new Intent(MenuActivity.this, CalendarController.class);
+				intent.putExtra("UserId", personIdLoggedPerson);
+				startActivity(intent);
+				return true;
+	
+			case R.id.NOTIFICATIONS:
+				intent = new Intent(MenuActivity.this, NotificationController.class);
+				intent.putExtra("UserId", personIdLoggedPerson);
+				startActivity(intent);
+				return true;
+	
+			case R.id.PROFIL:
+				intent = new Intent(MenuActivity.this, ProfilController.class);
+				intent.putExtra("UserId", personIdLoggedPerson);
+				startActivity(intent);
+				return true;
+	
+			case R.id.NOTIFICATION_SETTINGS:
+				intent = new Intent(MenuActivity.this, NotificationController.class);
+				intent.putExtra("UserId", personIdLoggedPerson);
+				intent.putExtra("Notification_Settings", "true");
+				startActivity(intent);
+				return true;
+			
+			case R.id.CHANGE_PRIVATE_INFORMATION:
+				intent = new Intent(MenuActivity.this, PrivateInfoController.class);
+				intent.putExtra("UserId", personIdLoggedPerson);
+				startActivity(intent);
+				return true;
+				
+			case R.id.LIST_EVENT_HISTORY:
+				intent = new Intent(MenuActivity.this, EventHistoryController.class);
+				intent.putExtra("UserId", personIdLoggedPerson);
+				startActivity(intent);
+				return true;
+				
+			case R.id.CHANGE_SECURITY_INFORMATION:
+				intent = new Intent(MenuActivity.this, SecurityInfoController.class);
+				intent.putExtra("UserId", personIdLoggedPerson);
+				startActivity(intent);
+				return true;
+	
+			default:
+				return false;
 		}
 	}
 }
