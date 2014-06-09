fe9823496fdc17c292a416026f16998e9b38feb0
oliverneubauer77@gmail.com <oliverneubauer77@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-06-09 12:40:47 +0000

    Add new Controller for further views

---
+++ src/fhkl/de/orgapp/controller/EventHistoryController.java
@@ -0,0 +1,16 @@
+package fhkl.de.orgapp.controller;
+
+import android.os.Bundle;
+
+import com.example.orgapp.R;
+
+import fhkl.de.orgapp.util.MenuActivity;
+
+public class EventHistoryController extends MenuActivity
+{
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.event_history);
+	}
+}
