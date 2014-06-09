b9e0a6a8acf8f00af84ea5e34995396555c94c0e
oliverneubauer77@gmail.com <oliverneubauer77@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-06-09 12:39:55 +0000

    new message for profil

--- src/fhkl/de/orgapp/util/IMessages.java
+++ src/fhkl/de/orgapp/util/IMessages.java
@@ -8,4 +8,5 @@ public interface IMessages
 	public static final String CREATING_PERSON = "Creating Person...";
 	public static final String LOADING_CALENDAR = "Loading Calendar...";
 	public static final String LOADING_NOTIFICATIONS = "Loading Notifications...";
+	public static final String LOADING_PROFIL = "Loading Profil...";
 }
