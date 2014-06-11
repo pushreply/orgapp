45952e616dfe192ba615ae5efea8c30e8b8c0ecf
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-06-11 21:15:34 +0000

    noch im arbeit, aber muss leider committen, da ich mit meinem festplatte nicht so vertraulich bin..

---
+++ src/fhkl/de/orgapp/controller/GroupController.java
@@ -0,0 +1,133 @@
+package fhkl.de.orgapp.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+
+import fhkl.de.orgapp.R;
+import fhkl.de.orgapp.util.IMessages;
+import fhkl.de.orgapp.util.JSONParser;
+import fhkl.de.orgapp.util.MenuActivity;
+
+public class GroupController extends MenuActivity {
+
+	// Progress Dialog
+	private ProgressDialog pDialog;
+	
+	JSONParser jsonParser = new JSONParser();
+	ArrayList<HashMap<String, String>> groupList;
+
+	private static String URL_SELECT_MY_GROUP = "http://pushrply.com/select_my_group.php";
+	
+	//JSON nodes
+	private static final String TAG_SUCCESS = "success";
+	private static final String GROUP_ID = "groupid";
+	private static final String GROUP_NAME= "GROUPNAME";
+	private static final String GROUP_INFO= "GROUPINFO";
+	private static final String GROUP_PICTURE="GROUP_PICTURE_URL";
+	
+	JSONArray groups = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.groups);
+		
+		groupList = new ArrayList<HashMap<String, String>>();
+		new Group().execute();
+	}
+	
+	class Group extends AsyncTask<String, String, String>{
+
+		@Override
+		protected void onPreExecute()
+		{
+			super.onPreExecute();
+			pDialog = new ProgressDialog(GroupController.this);
+			pDialog.setMessage(IMessages.LOADING_GROUPS);
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(false);
+			pDialog.show();
+		}
+		
+		protected String doInBackground(String... params) {
+			List<NameValuePair> vp = new ArrayList<NameValuePair>();
+			vp.add(new BasicNameValuePair("personId", getIntent().getStringExtra("UserId")));
+			
+			JSONObject json = jsonParser.makeHttpRequest(URL_SELECT_MY_GROUP, "GET", vp);
+
+			Log.d("Groups: ", json.toString());
+			
+			try
+			{
+				int success = json.getInt(TAG_SUCCESS);
+				if (success == 1)
+				{
+					groups = json.getJSONArray("groups");
+
+					for (int i = 0; i < groups.length(); i++)
+					{
+						JSONObject c = groups.getJSONObject(i);
+
+						String groupId = c.getString("groupId");
+						String gname = c.getString("name");
+						String ginfo = c.getString("info");
+						String gpic = c.getString("picture");
+
+						HashMap<String, String> map = new HashMap<String, String>();
+						map.put(GROUP_ID, groupId);
+						map.put(GROUP_NAME, gname);
+						map.put(GROUP_INFO, ginfo);
+						map.put(GROUP_PICTURE, gpic);
+
+						groupList.add(map);
+					}
+				}
+				else
+				{
+
+				}
+			}
+			catch (JSONException e)
+			{
+				System.out.println("Error in UserData.doInBackground(String... arg0): " + e.getMessage());
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+		
+		protected void onPostExecute(String file_url) {
+			pDialog.dismiss();
+			runOnUiThread(new Runnable() {
+				public void run() {
+					ListAdapter adapter = new SimpleAdapter(GroupController.this,
+							groupList, R.layout.group_item,
+							new String[] {GROUP_NAME, GROUP_INFO, GROUP_PICTURE }, new int[] { R.id.GROUPITEM });
+					
+					//update listview
+					ListView groupList = (ListView) findViewById(android.R.id.list);
+					groupList.setAdapter(adapter);
+				}
+			});
+		}
+		
+	}
+	
+}
