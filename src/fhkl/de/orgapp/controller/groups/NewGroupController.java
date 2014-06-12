01d1a20f3c57d86e657cd005a3344576ab5c90eb
Elynsyon@gmail.com <Elynsyon@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-06-12 12:00:24 +0000

    new group #1

---
+++ src/fhkl/de/orgapp/controller/groups/NewGroupController.java
@@ -0,0 +1,15 @@
+package fhkl.de.orgapp.controller.groups;
+
+import android.os.Bundle;
+import fhkl.de.orgapp.R;
+import fhkl.de.orgapp.util.MenuActivity;
+
+public class NewGroupController extends MenuActivity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_group);
+	}
+}
+
--- src/fhkl/de/orgapp/util/MenuActivity.java
+++ src/fhkl/de/orgapp/util/MenuActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import fhkl.de.orgapp.R;
 import fhkl.de.orgapp.controller.calendar.CalendarController;
 import fhkl.de.orgapp.controller.groups.GroupController;
+import fhkl.de.orgapp.controller.groups.NewGroupController;
 import fhkl.de.orgapp.controller.notification.NotificationController;
 import fhkl.de.orgapp.controller.notification.NotificationSettingsController;
 import fhkl.de.orgapp.controller.profile.EventHistoryController;
@@ -16,22 +17,25 @@ import fhkl.de.orgapp.controller.profile.ProfileController;
 import fhkl.de.orgapp.controller.profile.SecurityInfoController;
 import fhkl.de.orgapp.controller.start.StartController;
 
-public class MenuActivity extends Activity
-{
+public class MenuActivity extends Activity {
 	private String personIdLoggedPerson;
 
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main_menu, menu);
-		
-		if(getIntent().getComponent().getClassName().equals(NotificationController.class.getName()))
-		{
+
+		if (getIntent().getComponent().getClassName()
+				.equals(GroupController.class.getName())) {
+			menu.findItem(R.id.NEW_GROUP).setVisible(true);
+		}
+
+		if (getIntent().getComponent().getClassName()
+				.equals(NotificationController.class.getName())) {
 			menu.findItem(R.id.NOTIFICATION_SETTINGS).setVisible(true);
 		}
-		
-		if(getIntent().getComponent().getClassName().equals(ProfileController.class.getName()))
-		{
+
+		if (getIntent().getComponent().getClassName()
+				.equals(ProfileController.class.getName())) {
 			menu.findItem(R.id.PROFIL_SETTINGS).setVisible(true);
 		}
 
@@ -39,68 +43,73 @@ public class MenuActivity extends Activity
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
+	public boolean onOptionsItemSelected(MenuItem item) {
 		Intent intent;
 		personIdLoggedPerson = getIntent().getStringExtra("UserId");
 
-		switch(item.getItemId())
-		{
-			case R.id.CALENDAR:
-				intent = new Intent(MenuActivity.this, CalendarController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-	
-			case R.id.NOTIFICATIONS:
-				intent = new Intent(MenuActivity.this, NotificationController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-	
-			case R.id.PROFIL:
-				intent = new Intent(MenuActivity.this, ProfileController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-	
-			case R.id.NOTIFICATION_SETTINGS:
-				intent = new Intent(MenuActivity.this, NotificationSettingsController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-			
-			case R.id.CHANGE_PRIVATE_INFORMATION:
-				intent = new Intent(MenuActivity.this, PrivateInfoController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-				
-			case R.id.LIST_EVENT_HISTORY:
-				intent = new Intent(MenuActivity.this, EventHistoryController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-				
-			case R.id.CHANGE_SECURITY_INFORMATION:
-				intent = new Intent(MenuActivity.this, SecurityInfoController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-				
-			case R.id.GROUPS:
-				intent = new Intent(MenuActivity.this, GroupController.class);
-				intent.putExtra("UserId", personIdLoggedPerson);
-				startActivity(intent);
-				return true;
-				
-			case R.id.LOGOUT:
-				intent = new Intent(MenuActivity.this, StartController.class);
-				startActivity(intent);
-				return true;
-	
-			default:
-				return false;
+		switch (item.getItemId()) {
+		case R.id.CALENDAR:
+			intent = new Intent(MenuActivity.this, CalendarController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.GROUPS:
+			intent = new Intent(MenuActivity.this, GroupController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.NEW_GROUP:
+			intent = new Intent(MenuActivity.this, NewGroupController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.NOTIFICATIONS:
+			intent = new Intent(MenuActivity.this, NotificationController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.PROFIL:
+			intent = new Intent(MenuActivity.this, ProfileController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.NOTIFICATION_SETTINGS:
+			intent = new Intent(MenuActivity.this,
+					NotificationSettingsController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.CHANGE_PRIVATE_INFORMATION:
+			intent = new Intent(MenuActivity.this, PrivateInfoController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.LIST_EVENT_HISTORY:
+			intent = new Intent(MenuActivity.this, EventHistoryController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.CHANGE_SECURITY_INFORMATION:
+			intent = new Intent(MenuActivity.this, SecurityInfoController.class);
+			intent.putExtra("UserId", personIdLoggedPerson);
+			startActivity(intent);
+			return true;
+
+		case R.id.LOGOUT:
+			intent = new Intent(MenuActivity.this, StartController.class);
+			startActivity(intent);
+			return true;
+
+		default:
+			return false;
 		}
 	}
 }
