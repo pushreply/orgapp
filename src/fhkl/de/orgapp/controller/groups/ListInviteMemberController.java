a5cc4d79f2f3ccf1b59307ccfec20dc57ac664a4
oliverneubauer77@gmail.com <oliverneubauer77@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-06-21 17:50:16 +0000

    view for invite new members to group per list defined

--- src/fhkl/de/orgapp/controller/groups/ListInviteMemberController.java
+++ src/fhkl/de/orgapp/controller/groups/ListInviteMemberController.java
@@ -7,125 +7,244 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.LinearLayout;
-import android.widget.LinearLayout.LayoutParams;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 import fhkl.de.orgapp.R;
 import fhkl.de.orgapp.util.IMessages;
 import fhkl.de.orgapp.util.JSONParser;
 import fhkl.de.orgapp.util.MenuActivity;
+import fhkl.de.orgapp.util.UserData;
 
-public class ListInviteMemberController extends MenuActivity {
+public class ListInviteMemberController extends MenuActivity
+{
+	private static String URL_GET_INVITE_MEMBER_LIST = "http://pushrply.com/get_invite_member_list.php";
 
-	private String personIdLoggedPerson;
-
+	Button inviteButton, cancelButton;
+	View horizontalLine;
 	private ProgressDialog pDialog;
 	JSONParser jsonParser = new JSONParser();
-	JSONArray invite_member_list = null;
-	ArrayList<HashMap<String, String>> inviteMemberList;
-
-	private Button bInvite;
-	private Button bCancel;
-	CheckBox member_list_checkbox;
-
-	LinearLayout invite_member_list_linearlayout, checkbox_layout;
-
-	private static String URL_GET_INVITE_MEMBER_LIST = "http://pushrply.com/get_invite_member_list.php";
+	List<HashMap<String, Object>> personList;
+	
+	// JSON nodes
 	private static final String TAG_SUCCESS = "success";
-
+	
+	private static final String TAG_TEXT_FIRST_NAME = "TEXT_FIRST_NAME";
+	private static final String TAG_FIRST_NAME = "FIRST_NAME";
+	private static final String TAG_TEXT_LAST_NAME = "TEXT_LAST_NAME";
+	private static final String TAG_LAST_NAME = "LAST_NAME";
+	private static final String TAG_TEXT_EMAIL = "TEXT_EMAIL";
+	private static final String TAG_EMAIL = "EMAIL";
+	private static final String TAG_IS_SELECTED = "IS_SELECTED";
+	
+	JSONArray persons = null;
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.invite_member_checkbox);
-
-		personIdLoggedPerson = getIntent().getStringExtra("UserId");
-		invite_member_list_linearlayout = (LinearLayout) findViewById(R.id.INVITE_MEMBER_LIST_LINEARLAYOUT);
-		checkbox_layout = new LinearLayout(ListInviteMemberController.this);
-		LayoutParams params = (LinearLayout.LayoutParams) checkbox_layout
-				.getLayoutParams();
-		params.width = LayoutParams.MATCH_PARENT;
-		params.height = LayoutParams.WRAP_CONTENT;
-		checkbox_layout.setLayoutParams(params);
-		checkbox_layout.setOrientation(LinearLayout.VERTICAL);
-
-		invite_member_list_linearlayout.addView(checkbox_layout);
-		new GetList().execute();
+		setContentView(R.layout.invite_member_list);
+		
+		personList = new ArrayList<HashMap<String, Object>>();
+		
+		new PersonListGetter().execute();
 	}
 
-	class GetList extends AsyncTask<String, String, String> {
-
+	class PersonListGetter extends AsyncTask<String, String, String>
+	{
 		@Override
-		protected void onPreExecute() {
+		protected void onPreExecute()
+		{
 			super.onPreExecute();
+			
 			pDialog = new ProgressDialog(ListInviteMemberController.this);
 			pDialog.setMessage(IMessages.LOADING_LIST);
 			pDialog.setIndeterminate(false);
-			pDialog.setCancelable(false);
+			pDialog.setCancelable(true);
 			pDialog.show();
 		}
 
-		protected String doInBackground(String... args) {
-
+		protected String doInBackground(String... args)
+		{
 			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			params.add(new BasicNameValuePair("personId", personIdLoggedPerson));
-			JSONObject json = jsonParser.makeHttpRequest(URL_GET_INVITE_MEMBER_LIST,
-					"GET", params);
+			params.add(new BasicNameValuePair("personId", UserData.getPERSONID()));
+			JSONObject json = jsonParser.makeHttpRequest(URL_GET_INVITE_MEMBER_LIST, "GET", params);
 			int success;
-			try {
+			
+			try
+			{
 				success = json.getInt(TAG_SUCCESS);
-				if (success == 1) {
-					invite_member_list = json.getJSONArray("memberlist");
-
-					for (int i = 0; i < invite_member_list.length(); i++) {
-						JSONObject c = invite_member_list.getJSONObject(i);
-						System.out.println(i);
-						member_list_checkbox = new CheckBox(ListInviteMemberController.this);
-						System.out.println(member_list_checkbox);
-						LayoutParams par = (LinearLayout.LayoutParams) member_list_checkbox
-								.getLayoutParams();
-						par.width = LayoutParams.MATCH_PARENT;
-						par.height = LayoutParams.WRAP_CONTENT;
-						member_list_checkbox.setLayoutParams(par);
-						// member_list_checkbox.setChecked(false);
-						// member_list_checkbox.setVisibility(View.VISIBLE);
-						// System.out.println(c.getString("firstName") + " "
-						// + c.getString("lastName") + " " + c.getString("eMail"));
-						// member_list_checkbox.setId(c.getInt("personId"));
-						// member_list_checkbox.setText(c.getString("firstName") + " "
-						// + c.getString("lastName") + " " + c.getString("eMail"));
-						// System.out.println("test");
-						// System.out.println("id");
-						// System.out.println(invite_member_list_linearlayout);
-						// System.out.println("layout");
-						checkbox_layout.addView(member_list_checkbox);
-						System.out.println("added");
+				
+				if (success == 1)
+				{
+					persons = json.getJSONArray("memberList");
+					int p;
+					JSONObject person;
+					String firstName, lastName, email;
+					HashMap<String, Object> personMap;
+					
+					for(p = 0; p < persons.length(); p++)
+					{
+						person = persons.getJSONObject(p);
+						
+						firstName = person.getString("firstName");
+						lastName = person.getString("lastName");
+						email = person.getString("eMail");
+						
+						personMap = new HashMap<String, Object>();
+						
+						personMap.put(TAG_TEXT_FIRST_NAME, getString(R.string.TEXT_FIRST_NAME) + ": ");
+						personMap.put(TAG_FIRST_NAME, firstName);
+						personMap.put(TAG_TEXT_LAST_NAME, getString(R.string.TEXT_LAST_NAME) + ": ");
+						personMap.put(TAG_LAST_NAME, lastName);
+						personMap.put(TAG_TEXT_EMAIL, getString(R.string.TEXT_EMAIL) + ": ");
+						personMap.put(TAG_EMAIL, email);
+						personMap.put(TAG_IS_SELECTED, false);
+						
+						personList.add(personMap);
 					}
-				} else {
-
 				}
-			} catch (Exception e) {
-
+				else
+				{
+					//TODO error message
+				}
 			}
+			catch(JSONException e)
+			{
+				System.out.println("Error in ListInviteMemberController.PersonListGetter.doInBackground(String...): " + e.getMessage());
+				e.printStackTrace();
+			}
+			
 			return null;
 		}
 
-		protected void onPostExecute(String message) {
+		protected void onPostExecute(String message)
+		{
+			super.onPostExecute(message);
+			
 			pDialog.dismiss();
+			
+			loadLayout();
+			
+			runOnUiThread(new Runnable()
+			{
+				public void run()
+				{
+					ListAdapter adapter = new SimpleAdapter
+											(
+											   ListInviteMemberController.this,
+											   personList,
+											   R.layout.invite_member_list_item,
+											   new String[]
+													   	{
+													   		TAG_TEXT_FIRST_NAME,
+													   		TAG_FIRST_NAME,
+													   		TAG_TEXT_LAST_NAME,
+													   		TAG_LAST_NAME,
+													   		TAG_TEXT_EMAIL,
+													   		TAG_EMAIL
+													   	},
+											   new int[]
+													   	{
+													   		R.id.INVITE_MEMBER_LIST_TEXT_FIRST_NAME,
+													   		R.id.INVITE_MEMBER_LIST_USER_FIRST_NAME,
+													   		R.id.INVITE_MEMBER_LIST_TEXT_LAST_NAME,
+													   		R.id.INVITE_MEMBER_LIST_USER_LAST_NAME,
+													   		R.id.INVITE_MEMBER_LIST_TEXT_EMAIL,
+													   		R.id.INVITE_MEMBER_LIST_USER_EMAIL
+													   	}
+											);
 
+					// update listview
+					final ListView personViewList = (ListView) findViewById(android.R.id.list);
+					
+					personViewList.setAdapter(adapter);
+					
+					personViewList.setOnItemClickListener(new AdapterView.OnItemClickListener()
+					{
+						@Override
+						public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+						{
+							ImageView image = (ImageView) view.findViewById(R.id.INVITE_MEMBER_LIST_IMAGE_ACCEPT);
+							HashMap<String, Object> selectedPerson = getSelectedPerson((TextView) view.findViewById(R.id.INVITE_MEMBER_LIST_USER_EMAIL));
+							
+							if(image.getVisibility() == View.VISIBLE)
+							{
+								image.setVisibility(View.INVISIBLE);
+								
+								selectedPerson.put(TAG_IS_SELECTED, false);
+								personList.set(personList.indexOf(selectedPerson), selectedPerson);
+							}
+							else if(image.getVisibility() == View.INVISIBLE)
+							{
+								image.setVisibility(View.VISIBLE);
+								
+								selectedPerson.put(TAG_IS_SELECTED, true);
+								personList.set(personList.indexOf(selectedPerson), selectedPerson);
+							}
+						}
+						
+						private HashMap<String, Object> getSelectedPerson(TextView emailOfSelectedPerson)
+						{
+							HashMap<String, Object> selectedPerson = new HashMap<String, Object>();
+							int p;
+							
+							for(p=0; p<personList.size(); p++)
+								if(personList.get(p).get(TAG_EMAIL).equals(emailOfSelectedPerson.getText().toString()))
+									selectedPerson = personList.get(p);
+							
+							return selectedPerson;
+						}
+					});
+				}
+			});
+		}
+		
+		private void loadLayout()
+		{
+			getViews();
+			setTexts();
+			setBackgrounds();
+		}
+		
+		private void getViews()
+		{
+			inviteButton = (Button) findViewById(R.id.INVITE_MEMBER_LIST_INVITE_BUTTON);
+			cancelButton = (Button) findViewById(R.id.INVITE_MEMBER_LIST_CANCEL_BUTTON);
+			horizontalLine = findViewById(R.id.INVITE_MEMBER_LIST_HORIZONTAL_LINE);
+		}
+		
+		private void setTexts()
+		{
+			inviteButton.setText(getString(R.string.LIST_INVITE));
+			cancelButton.setText(getString(R.string.LIST_CANCEL));
+		}
+		
+		private void setBackgrounds()
+		{
+			inviteButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));		
+			cancelButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+			horizontalLine.setBackgroundColor(getResources().getColor(android.R.color.black));
 		}
 	}
 
-	class ListInviteMembers extends AsyncTask<String, String, String> {
-
+	class PersonListInvite extends AsyncTask<String, String, String>
+	{
 		@Override
-		protected void onPreExecute() {
+		protected void onPreExecute()
+		{
 			super.onPreExecute();
 			pDialog = new ProgressDialog(ListInviteMemberController.this);
 			pDialog.setMessage(IMessages.INVITING_MEMBER);
@@ -134,18 +253,19 @@ public class ListInviteMemberController extends MenuActivity {
 			pDialog.show();
 		}
 
-		protected String doInBackground(String... params) {
+		protected String doInBackground(String... params)
+		{
 			return null;
 		}
 
-		protected void onPostExecute(String message) {
+		protected void onPostExecute(String message)
+		{
 			pDialog.dismiss();
-			if (message != null) {
-				Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG)
-						.show();
-
+			
+			if(message != null)
+			{
+				Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
 			}
 		}
 	}
-
 }
--- src/fhkl/de/orgapp/db/get_invite_member_list.php
+++ src/fhkl/de/orgapp/db/get_invite_member_list.php
@@ -4,37 +4,46 @@
   * */
 $response = array ();
 
+if(!isset($_GET['personId']))
+{
+	$response ["success"] = 0;
+	echo json_encode($response);
+}
+
 require_once __DIR__ . '/db_connect.php';
 
 $db = new DB_CONNECT ();
 
 $personId = $_GET ['personId'];
-$groupId = $_GET ['groupId'];
-$result = mysql_query ( "select distinct priv.personId, pers.eMail, pers.firstName, pers.lastName
-		from privilege priv join person pers using (personId)
-		where priv.groupId in (select groupId from groups where personId = '$personId')
-		and priv.personId not like '$personId' and priv.groupId not like '$groupId'") or die ( mysql_error () );
 
-if (mysql_num_rows ( $result ) > 0) {
+$result = mysql_query("select * FROM person") or die (mysql_error());
 
-	$response ["memberlist"] = array ();
-	while ( $row = mysql_fetch_array ( $result ) ) {
-		$memberlist = array ();
+if(mysql_num_rows ( $result ) > 0)
+{
+	$response ["memberList"] = array();
+	
+	while($row = mysql_fetch_array($result))
+	{
+		$memberlist = array();
+		
 		$memberlist ["personId"] = $row ["personId"];
 		$memberlist ["eMail"] = html_entity_decode ( $row ["eMail"], ENT_QUOTES, 'UTF-8' );
 		$memberlist ["firstName"] = html_entity_decode ( $row ["firstName"], ENT_QUOTES, 'UTF-8' );
 		$memberlist ["lastName"] = html_entity_decode ( $row ["lastName"], ENT_QUOTES, 'UTF-8' );
 
 
-		array_push ( $response ["memberlist"], $memberlist );
+		array_push($response["memberList"], $memberlist);
 	}
+	
 	$response ["success"] = 1;
 
-	echo json_encode ( $response );
-} else {
+	echo json_encode($response);
+	
+}
+else
+{
 	$response ["success"] = 0;
-	$response ["message"] = "No memberlist found";
 
-	echo json_encode ( $response );
+	echo json_encode($response);
 }
 ?>
