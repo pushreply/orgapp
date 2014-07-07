d92136889c17b54bce2961ede88fd72802115864
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-07-07 20:38:58 +0000

    check internet connection availability.

--- src/fhkl/de/orgapp/controller/start/StartController.java
+++ src/fhkl/de/orgapp/controller/start/StartController.java
@@ -1,50 +1,74 @@
 package fhkl.de.orgapp.controller.start;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 import fhkl.de.orgapp.R;
 import fhkl.de.orgapp.controller.login.LoginController;
 import fhkl.de.orgapp.controller.registration.RegisterController;
+import fhkl.de.orgapp.util.IMessages;
 
 public class StartController extends Activity {
-  private Button bLogin;
-  private Button bRegister;
-  
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.start);
+	private Button bLogin;
+	private Button bRegister;
+	boolean isConnected;
 
-    addListenerOnButton();
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
 
-  }
+		addListenerOnButton();
 
-  public void addListenerOnButton() {
+	}
 
-    bLogin = (Button) findViewById(R.id.LOGIN);
-    bRegister = (Button) findViewById(R.id.REGISTER);
+	public void addListenerOnButton() {
+		
+		//check internet connection
+		Context ctx = this;
+		ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-    bLogin.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
+		NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+		isConnected = activeNetwork != null	&& activeNetwork.isConnectedOrConnecting();
+		
+		bLogin = (Button) findViewById(R.id.LOGIN);
+		bRegister = (Button) findViewById(R.id.REGISTER);
 
-        Intent i = new Intent(StartController.this, LoginController.class);
-        startActivity(i);
-      }
-    });
-    
-    bRegister.setOnClickListener(new OnClickListener() {
+		bLogin.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (isConnected == true) {
+					Intent i = new Intent(StartController.this,
+							LoginController.class);
+					startActivity(i);
+				} 
+				else 
+				{
+					Toast.makeText(getApplicationContext(), IMessages.NO_INTERNET_CONNECTION, Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 
-      @Override
-      public void onClick(View v) {
-
-        Intent i = new Intent(StartController.this, RegisterController.class);
-        startActivity(i);
-      }
-    });
-  }
+		bRegister.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (isConnected == true) {
+					Intent i = new Intent(StartController.this, RegisterController.class);
+					startActivity(i);
+				} 
+				else 
+				{
+					Toast.makeText(getApplicationContext(), IMessages.NO_INTERNET_CONNECTION, Toast.LENGTH_LONG).show();
+				}
+			}
+		});
+	}
 }
