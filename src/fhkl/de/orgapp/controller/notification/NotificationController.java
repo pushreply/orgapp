33f16fb8da734c5b8493961040a979aeaf1b1935
Elynsyon@gmail.com <Elynsyon@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-07-10 12:44:10 +0000

    expand/collapse textview notification

--- src/fhkl/de/orgapp/controller/notification/NotificationController.java
+++ src/fhkl/de/orgapp/controller/notification/NotificationController.java
@@ -10,13 +10,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import fhkl.de.orgapp.R;
 import fhkl.de.orgapp.util.IMessages;
 import fhkl.de.orgapp.util.JSONParser;
@@ -45,6 +52,11 @@ public class NotificationController extends MenuActivity {
 		setContentView(R.layout.notification);
 		notificationList = new ArrayList<HashMap<String, String>>();
 		new Notification().execute();
+
+		TextView message = (TextView) findViewById(R.id.MESSAGE);
+		if (Integer.valueOf(android.os.Build.VERSION.SDK_INT) < 16) {
+			message.setMaxLines(Integer.MAX_VALUE);
+		}
 	}
 
 	class Notification extends AsyncTask<String, String, String> {
@@ -67,8 +79,7 @@ public class NotificationController extends MenuActivity {
 			List<NameValuePair> params = new ArrayList<NameValuePair>();
 			params.add(new BasicNameValuePair("personId", UserData.getPERSONID()));
 
-			JSONObject json = jsonParser.makeHttpRequest(
-					url_get_notification_settings, "GET", params);
+			JSONObject json = jsonParser.makeHttpRequest(url_get_notification_settings, "GET", params);
 
 			Log.d("NotificationSettings: ", json.toString());
 
@@ -76,78 +87,62 @@ public class NotificationController extends MenuActivity {
 				int success = json.getInt(TAG_SUCCESS);
 				if (success == 1) {
 					List<NameValuePair> paramsNotifications = new ArrayList<NameValuePair>();
-					paramsNotifications.add(new BasicNameValuePair("personId", UserData
-							.getPERSONID()));
+					paramsNotifications.add(new BasicNameValuePair("personId", UserData.getPERSONID()));
 
 					notificationSettings = json.getJSONArray("notificationSettings");
 					for (int i = 0; i < notificationSettings.length(); i++) {
 						JSONObject c = notificationSettings.getJSONObject(i);
 						String groupInvites = c.getInt("groupInvites") == 1 ? "1" : null;
 						if (groupInvites != null) {
-							paramsNotifications.add(new BasicNameValuePair("groupInvites",
-									groupInvites));
+							paramsNotifications.add(new BasicNameValuePair("groupInvites", groupInvites));
 						}
 						String groupEdited = c.getInt("groupEdited") == 1 ? "2" : null;
 						if (groupEdited != null) {
-							paramsNotifications.add(new BasicNameValuePair("groupEdited",
-									groupEdited));
+							paramsNotifications.add(new BasicNameValuePair("groupEdited", groupEdited));
 						}
 						String groupRemoved = c.getInt("groupRemoved") == 1 ? "3" : null;
 						if (groupRemoved != null) {
-							paramsNotifications.add(new BasicNameValuePair("groupRemoved",
-									groupRemoved));
+							paramsNotifications.add(new BasicNameValuePair("groupRemoved", groupRemoved));
 						}
 						String eventsAdded = c.getInt("eventsAdded") == 1 ? "4" : null;
 						if (eventsAdded != null) {
-							paramsNotifications.add(new BasicNameValuePair("eventsAdded",
-									eventsAdded));
+							paramsNotifications.add(new BasicNameValuePair("eventsAdded", eventsAdded));
 						}
 						String eventsEdited = c.getInt("eventsEdited") == 1 ? "5" : null;
 						if (eventsEdited != null) {
-							paramsNotifications.add(new BasicNameValuePair("eventsEdited",
-									eventsEdited));
+							paramsNotifications.add(new BasicNameValuePair("eventsEdited", eventsEdited));
 						}
 						String eventsRemoved = c.getInt("eventsRemoved") == 1 ? "6" : null;
 						if (eventsRemoved != null) {
-							paramsNotifications.add(new BasicNameValuePair("eventsRemoved",
-									eventsRemoved));
+							paramsNotifications.add(new BasicNameValuePair("eventsRemoved", eventsRemoved));
 						}
 						String commentsAdded = c.getInt("commentsAdded") == 1 ? "7" : null;
 						if (commentsAdded != null) {
-							paramsNotifications.add(new BasicNameValuePair("commentsAdded",
-									commentsAdded));
+							paramsNotifications.add(new BasicNameValuePair("commentsAdded", commentsAdded));
 						}
-						String commentsEdited = c.getInt("commentsEdited") == 1 ? "8"
-								: null;
+						String commentsEdited = c.getInt("commentsEdited") == 1 ? "8" : null;
 						if (commentsEdited != null) {
-							paramsNotifications.add(new BasicNameValuePair("commentsEdited",
-									commentsEdited));
+							paramsNotifications.add(new BasicNameValuePair("commentsEdited", commentsEdited));
 						}
-						String commentsRemoved = c.getInt("commentsRemoved") == 1 ? "9"
-								: null;
+						String commentsRemoved = c.getInt("commentsRemoved") == 1 ? "9" : null;
 						if (commentsRemoved != null) {
-							paramsNotifications.add(new BasicNameValuePair("commentsRemoved",
-									commentsRemoved));
+							paramsNotifications.add(new BasicNameValuePair("commentsRemoved", commentsRemoved));
 						}
-						String privilegeGiven = c.getInt("privilegeGiven") == 1 ? "10"
-								: null;
+						String privilegeGiven = c.getInt("privilegeGiven") == 1 ? "10" : null;
 						if (privilegeGiven != null) {
-							paramsNotifications.add(new BasicNameValuePair("privilegeGiven",
-									privilegeGiven));
+							paramsNotifications.add(new BasicNameValuePair("privilegeGiven", privilegeGiven));
 						}
 
 						try {
 							Integer shownEntries = c.getInt("shownEntries");
-							paramsNotifications.add(new BasicNameValuePair("shownEntries",
-									shownEntries.toString()));
+							paramsNotifications.add(new BasicNameValuePair("shownEntries", shownEntries.toString()));
 						} catch (JSONException e) {
 
 						}
 
 					}
 
-					json = jsonParser.makeHttpRequest(url_get_notification, "GET",
-							paramsNotifications);
+					json = jsonParser.makeHttpRequest(url_get_notification, "GET", paramsNotifications);
 					if (success == 1) {
 						notification = json.getJSONArray("notification");
 
@@ -178,10 +173,65 @@ public class NotificationController extends MenuActivity {
 			pDialog.dismiss();
 			runOnUiThread(new Runnable() {
 				public void run() {
-					ListAdapter adapter = new SimpleAdapter(NotificationController.this,
-							notificationList, R.layout.notification_item,
-							new String[] { TAG_MESSAGE }, new int[] { R.id.MESSAGE });
+					ListAdapter adapter = new SimpleAdapter(NotificationController.this, notificationList,
+									R.layout.notification_item, new String[] { TAG_MESSAGE }, new int[] { R.id.MESSAGE });
 					ListView notificationList = (ListView) findViewById(android.R.id.list);
+
+					notificationList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+						@SuppressLint("NewApi")
+						@Override
+						public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+							final TextView message = (TextView) view.findViewById(R.id.MESSAGE);
+
+							final Animation slideDown = AnimationUtils.loadAnimation(NotificationController.this, R.anim.slide_down);
+							final Animation slideUp = AnimationUtils.loadAnimation(NotificationController.this, R.anim.slide_up);
+
+							slideDown.setAnimationListener(new AnimationListener() {
+
+								@Override
+								public void onAnimationStart(Animation animation) {
+
+									message.setMaxLines(Integer.MAX_VALUE);
+								}
+
+								@Override
+								public void onAnimationRepeat(Animation animation) {
+								}
+
+								@Override
+								public void onAnimationEnd(Animation animation) {
+								}
+							});
+
+							slideUp.setAnimationListener(new AnimationListener() {
+
+								@Override
+								public void onAnimationStart(Animation animation) {
+								}
+
+								@Override
+								public void onAnimationRepeat(Animation animation) {
+								}
+
+								@Override
+								public void onAnimationEnd(Animation animation) {
+									message.setMaxLines(2);
+								}
+
+							});
+
+							if (Integer.valueOf(android.os.Build.VERSION.SDK_INT) >= 16) {
+								if (message.getMaxLines() == 2) {
+									message.startAnimation(slideDown);
+								} else {
+									message.startAnimation(slideUp);
+								}
+							}
+
+						}
+					});
 					notificationList.setAdapter(adapter);
 				}
 			});
