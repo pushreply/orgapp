36504039fec3509f3df06676351a01d2424e7528
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-07-18 02:19:08 +0000

    add comment via php, using PHP PDOStatement

---
+++ src/fhkl/de/orgapp/db/pdo/pdo_db_connect.inc.php
@@ -0,0 +1,14 @@
+<?php
+
+try 
+{
+    $pdo = new PDO('mysql:host=localhost;dbname=orgapp_orgappfhzwdb', 'orgapp_fhzwdb', 'DbTk55ggRe');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->exec('SET NAMES "utf8"');
+} catch(PDOException $e) {
+	echo 'DB: FAILED';
+    exit();
+}
+echo 'DB: OK. ';
+
+?>
