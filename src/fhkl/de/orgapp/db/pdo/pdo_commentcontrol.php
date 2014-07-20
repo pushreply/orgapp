b357b985b307e4e4ba16eae281f9592c331b90bb
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-07-20 02:14:25 +0000

    insert, delete, select

--- src/fhkl/de/orgapp/db/pdo/pdo_commentcontrol.php
+++ src/fhkl/de/orgapp/db/pdo/pdo_commentcontrol.php
@@ -63,7 +63,7 @@ if ($_GET['addcomment']==1 && isset($_GET['message']))
 }
 
 /*------------------------------------------------------------
- * DELETE COMMENT
+ * DELETE 1 COMMENT
  * check the user input:
  * if button delete comment clicked, set deletecomment = 1
  * 
@@ -71,21 +71,26 @@ if ($_GET['addcomment']==1 && isset($_GET['message']))
  * and how we control them?
  */
 
-if ($_GET['deletecomment']==1 && isset($_GET['commentid'])) 
+if ($_GET['deletecomment']==1 && isset($_GET['commentid']) && isset($_GET['eventid']) && isset($_GET['personid'])) 
 {
+	
 	$commentid = $_GET['commentid'];
 	$eventid = $_GET['eventid'];
 	$personid= $_GET['personid'];
 	$classification = $_GET['classification'];
+	$groupid = $_GET['groupid'];
 	
 	$response = array ();
 	
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/' . $dbpath;
 	
 	try {
-		$sql = 'DELETE FROM comment WHERE commentid = :commentid';
+		
+		$sql = 'DELETE FROM comment WHERE commentid = :commentid AND eventid = :eventid AND personid = :personid';
 		$sth = $pdo->prepare($sql);
 		$sth->bindValue(':commentid', $commentid, PDO::PARAM_INT); /* integer must have "PDO::PARAM_INT"; it's a PHP PDO bug :)*/
+		$sth->bindValue(':eventid', $eventid, PDO::PARAM_INT);
+		$sth->bindValue(':personid', $groupid, PDO::PARAM_INT);
 		$confirm = $sth->execute();
 		
 		//check deletion status
@@ -105,5 +110,68 @@ if ($_GET['deletecomment']==1 && isset($_GET['commentid']))
 	}
 }
 	
-	
+/*------------------------------------------------------------
+ * SHOW COMMENTS
+* check the user input:
+* show comments if an event activity opened, set showcomment = 1
+* 
+*/
+
+if ($_GET['showcomment']==1 && isset($_GET['eventid']))
+{
+	$eventid = $_GET['eventid'];
+	$response = array ();
+
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/' . $dbpath;
+
+	try {
+
+		$sql = 'SELECT c.commentid, c.commentDateTime, c.personid, p.firstName, p.lastName, c.message 
+				FROM comment c, person p 
+				WHERE c.personid = p.personid
+				AND c.eventid = :eventid';
+		
+		$sth = $pdo->prepare($sql);
+		$sth->bindValue(':eventid', $eventid, PDO::PARAM_INT);
+		$confirm = $sth->execute();
+		$result = $sth->fetchAll();
+		
+		if ($confirm==true) {
+			/*
+			 * need a container for json
+			 */
+			$response["comment"] = array();
+			
+			/*
+			 * must be placed outside foreach loop, otherwise create a weird array index.
+			 */
+			$comment[] = array();
+			
+			foreach ($result as $row)
+			{
+				$comment['commentid'] = $row['commentid'];
+				$comment['commentdatetime'] = $row['commentDateTime'];
+				$comment['personid'] = $row['personid'];
+				$comment['firstname'] = html_entity_decode($row['firstName'], ENT_QUOTES, 'UTF-8');
+				$comment['lastname'] = html_entity_decode($row['lastName'], ENT_QUOTES, 'UTF-8');
+				$comment['message'] = html_entity_decode($row['message'], ENT_QUOTES, 'UTF-8');
+				
+				/*
+				 * push each value to the data container 
+				 */
+				array_push($response["comment"], $comment);
+			}
+			$response ["success"] = 1;
+			echo json_encode ($response);
+		}
+		else {
+			$response ["success"] = 0;
+			echo json_encode ($response);
+		}
+	} catch (Exception $e) {
+		echo 'ERROR: DB.';
+		exit();
+	}
+}
+
 ?>
