380723a1308a9b354a7dcb05bad7e39155f36027
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-07-21 14:31:20 +0000

    commentdata container

---
+++ src/fhkl/de/orgapp/util/CommentData.java
@@ -0,0 +1,49 @@
+package fhkl.de.orgapp.util;
+
+public class CommentData {
+	private static String COMMENTID = "";
+	private static String PERSONID = "";
+	private static String EVENTID = "";
+	private static String COMMENT = "";
+	private static String CLASSIFICATION = "";
+	private static boolean BACK = false;
+	
+	public static String getCOMMENTID() {
+		return COMMENTID;
+	}
+	public static void setCOMMENTID(String cOMMENTID) {
+		COMMENTID = cOMMENTID;
+	}
+	public static String getPERSONID() {
+		return PERSONID;
+	}
+	public static void setPERSONID(String pERSONID) {
+		PERSONID = pERSONID;
+	}
+	public static String getEVENTID() {
+		return EVENTID;
+	}
+	public static void setEVENTID(String eVENTID) {
+		EVENTID = eVENTID;
+	}
+	public static String getCOMMENT() {
+		return COMMENT;
+	}
+	public static void setCOMMENT(String cOMMENT) {
+		COMMENT = cOMMENT;
+	}
+	public static String getCLASSIFICATION() {
+		return CLASSIFICATION;
+	}
+	public static void setCLASSIFICATION(String cLASSIFICATION) {
+		CLASSIFICATION = cLASSIFICATION;
+	}
+	public static boolean isBACK() {
+		return BACK;
+	}
+	public static void setBACK(boolean bACK) {
+		BACK = bACK;
+	}
+	
+	
+}
