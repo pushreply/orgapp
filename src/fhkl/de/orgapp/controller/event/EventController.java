1508e42add6af319567d1786e3bdf45e8eae0f89
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-07-25 16:12:47 +0000

    showing comment.

--- src/fhkl/de/orgapp/controller/event/EventController.java
+++ src/fhkl/de/orgapp/controller/event/EventController.java
@@ -1,10 +1,12 @@
 package fhkl.de.orgapp.controller.event;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,9 +16,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import fhkl.de.orgapp.R;
+import fhkl.de.orgapp.util.CommentData;
 import fhkl.de.orgapp.util.EventData;
 import fhkl.de.orgapp.util.IMessages;
 import fhkl.de.orgapp.util.JSONParser;
@@ -32,6 +38,7 @@ public class EventController extends MenuActivity {
 	private static String URL_CREATE_PERSON_IN_EVENT = "http://pushrply.com/create_person_in_event.php";
 	private static String URL_DELETE_PERSON_IN_EVENT = "http://pushrply.com/delete_person_in_event.php";
 
+
 	private static final String TAG_SUCCESS = "success";
 
 	TextView eventTime;
@@ -40,6 +47,24 @@ public class EventController extends MenuActivity {
 
 	ToggleButton buttonAttendance;
 	boolean toggleButtonChecked;
+
+	//commentcontrol
+	private static String URL_COMMENTCONTROL = "http://pushrply.com/pdo_commentcontrol.php";
+	
+	private static final String TAG_COMMENT_ID = "COMMENTID";
+	private static final String TAG_PERSON_ID = "PERSONID";
+	private static final String TAG_FIRSTNAME = "FIRSTNAME";
+	private static final String TAG_LASTNAME = "LASTNAME";
+	private static final String TAG_MESSAGE = "MESSAGE";
+	private static final String TAG_COMMENTDATETIME = "COMMENTDATETIME";
+	
+	TextView messageContent;
+	TextView messageDateTime;
+	TextView firstname;
+	TextView lastname;
+	
+	ArrayList<HashMap<String, String>> commentList;
+	JSONArray comment = null;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +79,7 @@ public class EventController extends MenuActivity {
 		eventTime.setText("Time: " + EventData.getEVENTTIME());
 		eventDate.setText("Date: " + EventData.getEVENTDATE());
 		eventLocation.setText("Location: " + EventData.getEVENTLOCATION());
+		
 		buttonAttendance = (ToggleButton) findViewById(R.id.BUTTONATTENDANCE);
 		buttonAttendance.setOnClickListener(new OnClickListener() {
 
@@ -64,8 +90,10 @@ public class EventController extends MenuActivity {
 				new ChangeAttendingStatus().execute();
 			}
 		});
-
 		new GetEvent().execute();
+
+		commentList = new ArrayList<HashMap<String, String>>();
+		new ShowComments().execute();
 	}
 
 	class GetEvent extends AsyncTask<String, String, String> {
@@ -148,6 +176,97 @@ public class EventController extends MenuActivity {
 
 		protected void onPostExecute(String message) {
 			pDialog.dismiss();
+		}
+	}
+	
+	//Show comments
+	class ShowComments extends AsyncTask<String, String, String>
+	{
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(EventController.this);
+
+			if (getIntent().getStringExtra("Refresh") != null)
+				pDialog.setMessage(IMessages.UPDATING);
+			else
+				pDialog.setMessage(IMessages.LOADING_COMMENTS);
+
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+		}
+
+
+		@Override
+		protected String doInBackground(String... params) {
+			List<NameValuePair> vp = new ArrayList<NameValuePair>();
+			
+			CommentData.setACTION("showcomment");
+			vp.add(new BasicNameValuePair("do", CommentData.getACTION()));
+			vp.add(new BasicNameValuePair("eventId", EventData.getEVENTID()));
+			
+			System.out.println("EventData.getEVENTID() : " + EventData.getEVENTID());
+			
+			JSONObject json = jsonParser.makeHttpRequest(URL_COMMENTCONTROL, "GET", vp);
+
+			Log.d("Comments: ", json.toString());
+
+			try {
+				int success = json.getInt(TAG_SUCCESS);
+				if (success == 1) {
+					comment = json.getJSONArray("comment");
+
+					for (int i = 0; i < comment.length(); i++) {
+						
+						JSONObject c = comment.getJSONObject(i);
+
+						String commentId = c.getString("commentId");
+						String personId = c.getString("personId");
+						String firstname = c.getString("firstName");
+						String lastname = c.getString("lastName");
+						String commentdatetime = c.getString("commentDateTime");
+						String message = c.getString("message");
+
+						HashMap<String, String> map = new HashMap<String, String>();
+						map.put(TAG_COMMENT_ID, commentId);
+						map.put(TAG_PERSON_ID, personId);
+						map.put(TAG_MESSAGE, message);
+						map.put(TAG_FIRSTNAME, firstname);
+						map.put(TAG_LASTNAME, lastname);
+						map.put(TAG_COMMENTDATETIME, commentdatetime);
+						
+						commentList.add(map);
+					}
+				} else {
+
+				}
+			} catch (JSONException e) {
+				System.out.println("Error in CommentData.doInBackground(String... arg0): " + e.getMessage());
+				e.printStackTrace();
+			}
+
+			return null;
+		}
+		protected void onPostExecute(String file_url) {
+			pDialog.dismiss();
+			runOnUiThread(new Runnable() {
+				public void run() {
+					ListAdapter adapter = new SimpleAdapter(EventController.this, commentList, R.layout.comment_item,
+									new String[] {TAG_MESSAGE, 
+													TAG_FIRSTNAME, 
+													TAG_LASTNAME, 
+													TAG_COMMENTDATETIME}, 
+													new int[] { 
+															R.id.MESSAGE, 
+															R.id.FIRSTNAME, 
+															R.id.LASTNAME, 
+															R.id.COMMENTDATETIME}
+									);
+					// updating listview
+					ListView commentList = (ListView) findViewById(android.R.id.list);
+					commentList.setAdapter(adapter);
+				}
+			});
 		}
 	}
 }
