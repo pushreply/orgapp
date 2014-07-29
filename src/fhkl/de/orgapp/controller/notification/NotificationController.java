0aaecf12bebc0e3646b0f660944d8acfc5e6d24e
Elynsyon@gmail.com <Elynsyon@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-07-29 15:17:02 +0000

    TextAppearance Medium

--- src/fhkl/de/orgapp/controller/notification/NotificationController.java
+++ src/fhkl/de/orgapp/controller/notification/NotificationController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
@@ -33,8 +32,7 @@ import fhkl.de.orgapp.util.MenuActivity;
 import fhkl.de.orgapp.util.data.NotificationSettingsData;
 import fhkl.de.orgapp.util.data.UserData;
 
-public class NotificationController extends MenuActivity
-{
+public class NotificationController extends MenuActivity {
 	private ProgressDialog pDialog;
 	JSONParser jsonParser = new JSONParser();
 	ArrayList<HashMap<String, String>> notificationList;
@@ -50,17 +48,18 @@ public class NotificationController extends MenuActivity
 	JSONArray notification = null;
 	JSONArray notificationSettings = null;
 
+	@SuppressLint("InflateParams")
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		deleteIcon();
-		
+
 		setContentView(R.layout.notification);
 		notificationList = new ArrayList<HashMap<String, String>>();
 		new Notification().execute();
 
-		LayoutInflater inflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View notificationItemView = inflater.inflate(R.layout.notification_item, null);
 		TextView message = (TextView) notificationItemView.findViewById(R.id.MESSAGE);
 
@@ -85,83 +84,70 @@ public class NotificationController extends MenuActivity
 			pDialog.show();
 		}
 
-		protected String doInBackground(String... args)
-		{
-			try
-			{
+		protected String doInBackground(String... args) {
+			try {
 				List<NameValuePair> paramsNotifications = new ArrayList<NameValuePair>();
 				paramsNotifications.add(new BasicNameValuePair("personId", UserData.getPERSONID()));
-				
+
 				String groupInvites = Boolean.parseBoolean(NotificationSettingsData.getGROUP_INVITES()) ? "1" : null;
-				if (groupInvites != null)
-				{
+				if (groupInvites != null) {
 					paramsNotifications.add(new BasicNameValuePair("groupInvites", groupInvites));
 				}
 				String groupEdited = Boolean.parseBoolean(NotificationSettingsData.getGROUP_EDITED()) ? "2" : null;
-				if (groupEdited != null)
-				{
+				if (groupEdited != null) {
 					paramsNotifications.add(new BasicNameValuePair("groupEdited", groupEdited));
 				}
 				String groupRemoved = Boolean.parseBoolean(NotificationSettingsData.getGROUP_REMOVED()) ? "3" : null;
-				if (groupRemoved != null)
-				{
+				if (groupRemoved != null) {
 					paramsNotifications.add(new BasicNameValuePair("groupRemoved", groupRemoved));
 				}
 				String eventsAdded = Boolean.parseBoolean(NotificationSettingsData.getEVENTS_ADDED()) ? "4" : null;
-				if (eventsAdded != null)
-				{
+				if (eventsAdded != null) {
 					paramsNotifications.add(new BasicNameValuePair("eventsAdded", eventsAdded));
 				}
 				String eventsEdited = Boolean.parseBoolean(NotificationSettingsData.getEVENTS_EDITED()) ? "5" : null;
-				if (eventsEdited != null)
-				{
+				if (eventsEdited != null) {
 					paramsNotifications.add(new BasicNameValuePair("eventsEdited", eventsEdited));
 				}
 				String eventsRemoved = Boolean.parseBoolean(NotificationSettingsData.getEVENTS_REMOVED()) ? "6" : null;
-				if (eventsRemoved != null)
-				{
+				if (eventsRemoved != null) {
 					paramsNotifications.add(new BasicNameValuePair("eventsRemoved", eventsRemoved));
 				}
 				String commentsAdded = Boolean.parseBoolean(NotificationSettingsData.getCOMMENTS_ADDED()) ? "7" : null;
-				if (commentsAdded != null)
-				{
+				if (commentsAdded != null) {
 					paramsNotifications.add(new BasicNameValuePair("commentsAdded", commentsAdded));
 				}
 				String commentsEdited = Boolean.parseBoolean(NotificationSettingsData.getCOMMENTS_EDITED()) ? "8" : null;
-				if (commentsEdited != null)
-				{
+				if (commentsEdited != null) {
 					paramsNotifications.add(new BasicNameValuePair("commentsEdited", commentsEdited));
 				}
 				String commentsRemoved = Boolean.parseBoolean(NotificationSettingsData.getCOMMENTS_REMOVED()) ? "9" : null;
-				if (commentsRemoved != null)
-				{
+				if (commentsRemoved != null) {
 					paramsNotifications.add(new BasicNameValuePair("commentsRemoved", commentsRemoved));
 				}
 				String privilegeGiven = Boolean.parseBoolean(NotificationSettingsData.getPRIVILEGE_GIVEN()) ? "10" : null;
-				if (privilegeGiven != null)
-				{
+				if (privilegeGiven != null) {
 					paramsNotifications.add(new BasicNameValuePair("privilegeGiven", privilegeGiven));
 				}
-				
-				if(NotificationSettingsData.getSHOW_ENTRIES() != null && !NotificationSettingsData.getSHOW_ENTRIES().equals(""))
+
+				if (NotificationSettingsData.getSHOW_ENTRIES() != null
+								&& !NotificationSettingsData.getSHOW_ENTRIES().equals(""))
 					paramsNotifications.add(new BasicNameValuePair("shownEntries", NotificationSettingsData.getSHOW_ENTRIES()));
-				
+
 				JSONObject json = jsonParser.makeHttpRequest(url_get_notification, "GET", paramsNotifications);
-				
+
 				int success = json.getInt(TAG_SUCCESS);
-				
-				if (success == 1)
-				{
+
+				if (success == 1) {
 					notification = json.getJSONArray("notification");
-					
-					for (int i = 0; i < notification.length(); i++)
-					{
+
+					for (int i = 0; i < notification.length(); i++) {
 						JSONObject c = notification.getJSONObject(i);
-						
+
 						String id = c.getString("notificationsId");
 						String message = c.getString("message");
 						String isRead = c.getString("isRead");
-						
+
 						HashMap<String, String> map = new HashMap<String, String>();
 						map.put(TAG_ID, id);
 						map.put(TAG_MESSAGE, message);
@@ -170,13 +156,9 @@ public class NotificationController extends MenuActivity
 						notificationList.add(map);
 					}
 				}
-				else
-				{
-				}
-			}
-			catch (JSONException e)
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
+				logout();
 			}
 
 			return null;
@@ -186,49 +168,44 @@ public class NotificationController extends MenuActivity
 			pDialog.dismiss();
 			runOnUiThread(new Runnable() {
 				public void run() {
-					
-					ListView notificationListView = (ListView) findViewById(android.R.id.list);
-					
-					class NotificationListAdapter extends BaseAdapter
-					{
-				        private ArrayList<HashMap<String, String>> notificationList;
-				        
-				        public NotificationListAdapter(ArrayList<HashMap<String, String>> notificationList)
-				        {
-				            this.notificationList = notificationList;
-				        }
 
-				        public int getCount()
-				        {
-				            return notificationList.size();
-				        }
+					final ListView notificationListView = (ListView) findViewById(android.R.id.list);
 
-				        public Object getItem(int arg0)
-				        {
-				            return null;
-				        }
+					class NotificationListAdapter extends BaseAdapter {
+						private ArrayList<HashMap<String, String>> notificationList;
 
-				        public long getItemId(int position)
-				        {
-				            return position;
-				        }
+						public NotificationListAdapter(ArrayList<HashMap<String, String>> notificationList) {
+							this.notificationList = notificationList;
+						}
 
-				        public View getView(int position, View convertView, ViewGroup parent)
-				        {
-				            LayoutInflater inflater = getLayoutInflater();
-				            View row;
-				            row = inflater.inflate(R.layout.notification_item, parent, false);
-				            TextView message;
-				            message = (TextView) row.findViewById(R.id.MESSAGE);
-				            message.setText(notificationList.get(position).get(TAG_MESSAGE).toString());
-				            
-				            if(notificationList.get(position).get(TAG_IS_READ).equals("0"))
-				            	message.setTypeface(Typeface.DEFAULT_BOLD);
-				            
-				            return (row);
-				        }
-				    }
-					
+						public int getCount() {
+							return notificationList.size();
+						}
+
+						public Object getItem(int arg0) {
+							return null;
+						}
+
+						public long getItemId(int position) {
+							return position;
+						}
+
+						@SuppressLint("ViewHolder")
+						public View getView(int position, View convertView, ViewGroup parent) {
+							LayoutInflater inflater = getLayoutInflater();
+							View row;
+							row = inflater.inflate(R.layout.notification_item, parent, false);
+							TextView message;
+							message = (TextView) row.findViewById(R.id.MESSAGE);
+							message.setText(notificationList.get(position).get(TAG_MESSAGE).toString());
+
+							if (notificationList.get(position).get(TAG_IS_READ).equals("0"))
+								message.setTypeface(Typeface.DEFAULT_BOLD);
+
+							return (row);
+						}
+					}
+
 					notificationListView.setAdapter(new NotificationListAdapter(notificationList));
 					notificationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
@@ -237,13 +214,12 @@ public class NotificationController extends MenuActivity
 						public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
 							final TextView message = (TextView) view.findViewById(R.id.MESSAGE);
-							
-							if(message.getTypeface() != null && message.getTypeface().isBold())
-							{
+
+							if (message.getTypeface() != null && message.getTypeface().isBold()) {
 								message.setTypeface(Typeface.DEFAULT);
 								new NotificationReadStatusUpdater().execute(position);
 							}
-							
+
 							Animation slideDown = AnimationUtils.loadAnimation(NotificationController.this, R.anim.slide_down);
 							Animation slideUp = AnimationUtils.loadAnimation(NotificationController.this, R.anim.slide_up);
 
@@ -277,6 +253,7 @@ public class NotificationController extends MenuActivity
 								@Override
 								public void onAnimationEnd(Animation animation) {
 									message.setMaxLines(2);
+									notificationListView.invalidate();
 								}
 
 							});
@@ -289,16 +266,14 @@ public class NotificationController extends MenuActivity
 								}
 							}
 						}
-						
-						class NotificationReadStatusUpdater extends AsyncTask<Integer, String, String>
-						{
+
+						class NotificationReadStatusUpdater extends AsyncTask<Integer, String, String> {
 							@Override
-							protected String doInBackground(Integer... arg)
-							{
+							protected String doInBackground(Integer... arg) {
 								List<NameValuePair> params = new ArrayList<NameValuePair>();
 								params.add(new BasicNameValuePair("notificationsId", notificationList.get(arg[0]).get(TAG_ID)));
 								jsonParser.makeHttpRequest(url_update_notification_read_status, "GET", params);
-								
+
 								return null;
 							}
 						}
