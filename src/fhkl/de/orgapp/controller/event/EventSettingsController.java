803961352fea55b381ce425cf2ed333f9369bbf5
Elynsyon@gmail.com <Elynsyon@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-08-01 11:18:45 +0000

    warnings removed

--- src/fhkl/de/orgapp/controller/event/EventSettingsController.java
+++ src/fhkl/de/orgapp/controller/event/EventSettingsController.java
@@ -21,7 +21,6 @@ import fhkl.de.orgapp.R;
 import fhkl.de.orgapp.util.IMessages;
 import fhkl.de.orgapp.util.JSONParser;
 import fhkl.de.orgapp.util.MenuActivity;
-import fhkl.de.orgapp.util.data.NotificationSettingsData;
 import fhkl.de.orgapp.util.data.UserData;
 
 /**
@@ -29,223 +28,189 @@ import fhkl.de.orgapp.util.data.UserData;
  * 
  * @author Oliver Neubauer
  * @version 1.0
- *
+ * 
  */
 
-public class EventSettingsController extends MenuActivity
-{
+public class EventSettingsController extends MenuActivity {
 	private ProgressDialog pDialog;
 	JSONParser jsonParser = new JSONParser();
-	
+
 	private static String URL_UPDATE_EVENT_SETTINGS = "http://pushrply.com/pdo_person.php";
 	private static final String TAG_SUCCESS = "success";
-	
+
 	CheckBox receivedEntries;
 	EditText numberEntries;
 	Button save, cancel;
 	Integer shownEntries;
-	
+
 	/**
 	 * 
 	 * 
 	 * @param savedInstanceState contains the data
 	 */
-	
+
 	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.event_settings);
-		
+
 		receivedEntries = (CheckBox) findViewById(R.id.EVENT_SETTINGS_RECEIVED_EVENT_ENTRIES);
 		numberEntries = (EditText) findViewById(R.id.EVENT_SETTINGS_NUMBER_EVENT_ENTRIES);
 		save = (Button) findViewById(R.id.EVENT_SETTINGS_BUTTON_SAVE);
 		cancel = (Button) findViewById(R.id.EVENT_SETTINGS_BUTTON_CANCEL);
-		
-		if(UserData.getSHOWN_EVENT_ENTRIES() != null && !UserData.getSHOWN_EVENT_ENTRIES().equals(""))
-		{
+
+		if (UserData.getSHOWN_EVENT_ENTRIES() != null && !UserData.getSHOWN_EVENT_ENTRIES().equals("")) {
 			receivedEntries.setChecked(true);
 			numberEntries.setText(UserData.getSHOWN_EVENT_ENTRIES());
 			numberEntries.setVisibility(View.VISIBLE);
-		}
-		else
-		{
+		} else {
 			numberEntries.setVisibility(View.GONE);
 		}
-		
-		receivedEntries.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
-		{
+
+		receivedEntries.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-			{
-				if(isChecked)
-				{
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
 					numberEntries.setVisibility(View.VISIBLE);
-				}
-				else
-				{
+				} else {
 					numberEntries.setVisibility(View.GONE);
 					numberEntries.setText("");
 				}
 			}
 		});
 	}
-	
-	public void saveEventSettings(View view)
-	{
-		if(isNumberOfEntriesEmpty())
-		{
+
+	public void saveEventSettings(View view) {
+		if (isNumberOfEntriesEmpty()) {
 			Toast.makeText(getApplicationContext(), IMessages.Error.NO_NUMBER_ENTERED, Toast.LENGTH_LONG).show();
 			return;
 		}
-		
-		if(isNumberZero())
-		{
+
+		if (isNumberZero()) {
 			Toast.makeText(getApplicationContext(), IMessages.Error.NUMBER_ZERO_NOT_ALLOW, Toast.LENGTH_LONG).show();
 			return;
 		}
-		
-		if(!hasEventSettingsChanged())
-		{
+
+		if (!hasEventSettingsChanged()) {
 			Toast.makeText(getApplicationContext(), IMessages.Error.NO_CHANGES_MADE, Toast.LENGTH_LONG).show();
 			return;
 		}
-		
+
 		new EventSettingsSaver().execute();
 	}
-	
-	public void cancelEventSettings(View view)
-	{
+
+	public void cancelEventSettings(View view) {
 		Intent intent = new Intent(EventSettingsController.this, EventController.class);
 		startActivity(intent);
 	}
-	
-	private boolean isNumberOfEntriesEmpty()
-	{
-		if(receivedEntries.isChecked() && numberEntries.getText().toString().equals(""))
+
+	private boolean isNumberOfEntriesEmpty() {
+		if (receivedEntries.isChecked() && numberEntries.getText().toString().equals(""))
 			return true;
-		
+
 		return false;
 	}
-	
-	private boolean isNumberZero()
-	{
-		if(numberEntries.getText().toString().equals("0"))
+
+	private boolean isNumberZero() {
+		if (numberEntries.getText().toString().equals("0"))
 			return true;
-		
+
 		return false;
 	}
-	
-	private boolean hasEventSettingsChanged()
-	{
-		if(numberEntries.getText().toString().equals(UserData.getSHOWN_EVENT_ENTRIES()))
+
+	private boolean hasEventSettingsChanged() {
+		if (numberEntries.getText().toString().equals(UserData.getSHOWN_EVENT_ENTRIES()))
 			return false;
-		
+
 		return true;
 	}
-	
+
 	/**
 	 * EventSettingsSaver - Saves the event settings
 	 * 
 	 * @author Oliver Neubauer
 	 * @version 1.0
 	 */
-	
-	class EventSettingsSaver extends AsyncTask<String, String, String>
-	{
+
+	class EventSettingsSaver extends AsyncTask<String, String, String> {
 		/**
 		 * Defines a progress dialog within the main thread
 		 */
-		
+
 		@Override
-		protected void onPreExecute()
-		{
+		protected void onPreExecute() {
 			super.onPreExecute();
-			
+
 			pDialog = new ProgressDialog(EventSettingsController.this);
 			pDialog.setMessage(IMessages.Status.SAVING_SETTINGS);
 			pDialog.setIndeterminate(false);
 			pDialog.setCancelable(false);
 			pDialog.show();
 		}
-		
+
 		/**
 		 * Prepares and makes a http-request within the background thread
 		 * 
 		 * @param arg0 the arguments as String array
 		 */
-		
+
 		@Override
-		protected String doInBackground(String... arg0)
-		{
+		protected String doInBackground(String... arg0) {
 			List<NameValuePair> params = new ArrayList<NameValuePair>();
-			
+
 			params.add(new BasicNameValuePair("do", "updateeventsettings"));
 			params.add(new BasicNameValuePair("personId", UserData.getPERSONID()));
 
-			if(receivedEntries.isChecked())
-			{
-				try
-				{
+			if (receivedEntries.isChecked()) {
+				try {
 					shownEntries = Integer.valueOf(numberEntries.getText().toString());
-					
-					if (shownEntries > Integer.MAX_VALUE)
-					{
+
+					if (shownEntries > Integer.MAX_VALUE) {
 						return IMessages.Error.INVALID_NUMBER;
 					}
-				}
-				catch(NumberFormatException e)
-				{
+				} catch (NumberFormatException e) {
 					return IMessages.Error.INVALID_NUMBER;
 				}
-				
+
 				params.add(new BasicNameValuePair("shownEventEntries", numberEntries.getText().toString()));
 			}
-			
+
 			JSONObject json = jsonParser.makeHttpRequest(URL_UPDATE_EVENT_SETTINGS, "GET", params);
 
-			try
-			{
+			try {
 				int success = json.getInt(TAG_SUCCESS);
-				
-				if (success == 1)
-				{
+
+				if (success == 1) {
 					updateEventSettingsInUserData();
-					
+
 					return IMessages.Success.UPDATE_WAS_SUCCESSFUL;
 				}
-			}
-			catch (Exception e)
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
 				logout();
 			}
 
 			return null;
 		}
-		
+
 		/**
-		 * Displays an user message within main thread.
-		 * Goes to EventController
-		 *
+		 * Displays an user message within main thread. Goes to EventController
+		 * 
 		 * @param message the message to be displayed
 		 */
-		
-		protected void onPostExecute(String message)
-		{
+
+		protected void onPostExecute(String message) {
 			pDialog.dismiss();
 
-			if (message != null)
-			{
+			if (message != null) {
 				Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
-				
+
 				Intent intent = new Intent(EventSettingsController.this, EventController.class);
 				startActivity(intent);
 			}
 		}
-		
-		private void updateEventSettingsInUserData()
-		{
+
+		private void updateEventSettingsInUserData() {
 			UserData.setSHOWN_EVENT_ENTRIES(receivedEntries.isChecked() ? numberEntries.getText().toString() : "");
 		}
 	}
--- src/fhkl/de/orgapp/controller/login/LoginController.java
+++ src/fhkl/de/orgapp/controller/login/LoginController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
@@ -24,16 +23,14 @@ import fhkl.de.orgapp.controller.calendar.CalendarController;
 import fhkl.de.orgapp.controller.start.StartController;
 import fhkl.de.orgapp.util.IMessages;
 import fhkl.de.orgapp.util.JSONParser;
-import fhkl.de.orgapp.util.MenuActivity;
 import fhkl.de.orgapp.util.data.NotificationSettingsData;
 import fhkl.de.orgapp.util.data.UserData;
 import fhkl.de.orgapp.util.validator.OutputValidator;
 
-public class LoginController extends Activity
-{
+public class LoginController extends Activity {
 	// Progress Dialog
 	private ProgressDialog pDialog;
-	
+
 	private Button bSubmit;
 	private Button bCancel;
 
@@ -45,8 +42,7 @@ public class LoginController extends Activity
 	int success;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
 
@@ -56,178 +52,162 @@ public class LoginController extends Activity
 		inputEMail = (EditText) findViewById(R.id.EMAIL);
 		inputPassword = (EditText) findViewById(R.id.PASSWORD);
 
-		bSubmit.setOnClickListener(new OnClickListener()
-		{
+		bSubmit.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v)
-			{
+			public void onClick(View v) {
 				new Validator().execute();
 			}
 		});
 
-		bCancel.setOnClickListener(new OnClickListener()
-		{
+		bCancel.setOnClickListener(new OnClickListener() {
 			@Override
-			public void onClick(View v)
-			{
+			public void onClick(View v) {
 				Intent i = new Intent(LoginController.this, StartController.class);
 				startActivity(i);
 			}
 		});
 	}
 
-	class Validator extends AsyncTask<String, String, String>
-	{
+	class Validator extends AsyncTask<String, String, String> {
 		final static String TAG = "Validator";
-		
-		public Validator()
-		{}
-		
+
+		public Validator() {
+		}
+
 		@Override
-		protected void onPreExecute()
-		{
+		protected void onPreExecute() {
 			super.onPreExecute();
-			
+
 			pDialog = new ProgressDialog(LoginController.this);
 			pDialog.setMessage(IMessages.Status.CHECKING_DATA);
 			pDialog.setIndeterminate(false);
 			pDialog.setCancelable(true);
 			pDialog.show();
 		}
-		
+
 		@Override
-		protected String doInBackground(String... arg0)
-		{
+		protected String doInBackground(String... arg0) {
 			// url to select a person
 			String urlSelectPerson = "http://pushrply.com/select_person_by_email.php";
 			params = new ArrayList<NameValuePair>();
 			params.add(new BasicNameValuePair("eMail", inputEMail.getText().toString()));
-			
+
 			json = new JSONParser().makeHttpRequest(urlSelectPerson, "GET", params);
 
-			try
-			{
+			try {
 				success = json.getInt("success");
-				
-				if(success == 1)
-				{
+
+				if (success == 1) {
 					person = json.getJSONArray("person");
-					
+
 					e = person.getJSONObject(0);
-						
+
 					String eMail = e.getString("eMail");
-						
-					if (eMail.equals(inputEMail.getText().toString()))
-					{
+
+					if (eMail.equals(inputEMail.getText().toString())) {
 						String password = e.getString("password");
-						//verschlüsselung
-							
-						if (password.equals(inputPassword.getText().toString()))
-						{
-							//fetch notification settings of the user
+						// verschlüsselung
+
+						if (password.equals(inputPassword.getText().toString())) {
+							// fetch notification settings of the user
 							String urlSelectNotificationSettings = "http://pushrply.com/get_notification_settings.php";
 							params = new ArrayList<NameValuePair>();
 							params.add(new BasicNameValuePair("personId", e.getString("personId")));
 
 							json = new JSONParser().makeHttpRequest(urlSelectNotificationSettings, "GET", params);
-							
+
 							success = json.getInt("success");
-							
-							if(success == 1)
-							{
+
+							if (success == 1) {
 								notificationSettingsArray = json.getJSONArray("notificationSettings");
 								notificationSettings = notificationSettingsArray.getJSONObject(0);
-							}
-							else
-							{
+							} else {
 								System.out.println("No notificationSettings loaded");
 							}
-							
-							//invokes onPostExecute(String)
+
+							// invokes onPostExecute(String)
 							return null;
-						}
-						else
-						{
-							//invokes onPostExecute(String)
+						} else {
+							// invokes onPostExecute(String)
 							return IMessages.Error.INVALID_PASSWORD;
 						}
-					}
-					else
-					{
-						//invokes onPostExecute(String)
+					} else {
+						// invokes onPostExecute(String)
 						return IMessages.Error.INVALID_USER;
 					}
-				}				
-				else
-				{
-					//invokes onPostExecute(String)
+				} else {
+					// invokes onPostExecute(String)
 					return IMessages.Error.INVALID_USER;
 				}
-			}
-			catch (Exception e)
-			{
+			} catch (Exception e) {
 				e.printStackTrace();
 				logout();
 			}
-			
-			//invokes onPostExecute(String)
+
+			// invokes onPostExecute(String)
 			return null;
 		}
-		
+
 		@Override
-		protected void onPostExecute(String message)
-		{
+		protected void onPostExecute(String message) {
 			pDialog.dismiss();
-			
-			//error message
-			if(message != null)
-			{
+
+			// error message
+			if (message != null) {
 				Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
 			}
-			//everything successful
-			else
-			{
-				try
-				{
+			// everything successful
+			else {
+				try {
 					// set user data
 					UserData.setPERSONID(e.getString("personId"));
 					UserData.setFIRST_NAME(e.getString("firstName"));
 					UserData.setLAST_NAME(e.getString("lastName"));
-					UserData.setBIRTHDAY(OutputValidator.isUserBirthdaySet(e.getString("birthday")) ? e.getString("birthday") : "" );
+					UserData.setBIRTHDAY(OutputValidator.isUserBirthdaySet(e.getString("birthday")) ? e.getString("birthday")
+									: "");
 					UserData.setGENDER(e.getString("gender"));
 					UserData.setEMAIL(e.getString("eMail"));
 					UserData.setMEMBER_SINCE(e.getString("created"));
-					UserData.setSHOWN_EVENT_ENTRIES(OutputValidator.isPersonShownEventEntriesSet(e.getString("shownEventEntries")) ? e.getString("shownEventEntries") : "");
-					
+					UserData.setSHOWN_EVENT_ENTRIES(OutputValidator.isPersonShownEventEntriesSet(e.getString("shownEventEntries")) ? e
+									.getString("shownEventEntries") : "");
+
 					// set notification settings of the user
-					NotificationSettingsData.setNOTIFICATION_SETTINGS_ID(notificationSettings.getString("notificationSettingsId"));
-					NotificationSettingsData.setSHOW_ENTRIES(OutputValidator.isNotificationSettingsShownEntriesSet(notificationSettings.getString("shownEntries")) ? notificationSettings.getString("shownEntries") : "");
-					NotificationSettingsData.setGROUP_INVITES(notificationSettings.getInt("groupInvites") == 1 ? "true": "false");
-					NotificationSettingsData.setGROUP_EDITED(notificationSettings.getInt("groupEdited") == 1 ? "true": "false");
-					NotificationSettingsData.setGROUP_REMOVED(notificationSettings.getInt("groupRemoved") == 1 ? "true": "false");
-					NotificationSettingsData.setEVENTS_ADDED(notificationSettings.getInt("eventsAdded") == 1 ? "true": "false");
-					NotificationSettingsData.setEVENTS_EDITED(notificationSettings.getInt("eventsEdited") == 1 ? "true": "false");
-					NotificationSettingsData.setEVENTS_REMOVED(notificationSettings.getInt("eventsRemoved") == 1 ? "true": "false");
-					NotificationSettingsData.setCOMMENTS_ADDED(notificationSettings.getInt("commentsAdded") == 1 ? "true": "false");
-					NotificationSettingsData.setCOMMENTS_EDITED(notificationSettings.getInt("commentsEdited") == 1 ? "true": "false");
-					NotificationSettingsData.setCOMMENTS_REMOVED(notificationSettings.getInt("commentsRemoved") == 1 ? "true": "false");
-					NotificationSettingsData.setPRIVILEGE_GIVEN(notificationSettings.getInt("privilegeGiven") == 1 ? "true": "false");
-					NotificationSettingsData.setVIBRATION(notificationSettings.getInt("vibration") == 1 ? "true": "false");
-					
+					NotificationSettingsData
+									.setNOTIFICATION_SETTINGS_ID(notificationSettings.getString("notificationSettingsId"));
+					NotificationSettingsData
+									.setSHOW_ENTRIES(OutputValidator.isNotificationSettingsShownEntriesSet(notificationSettings
+													.getString("shownEntries")) ? notificationSettings.getString("shownEntries") : "");
+					NotificationSettingsData
+									.setGROUP_INVITES(notificationSettings.getInt("groupInvites") == 1 ? "true" : "false");
+					NotificationSettingsData.setGROUP_EDITED(notificationSettings.getInt("groupEdited") == 1 ? "true" : "false");
+					NotificationSettingsData
+									.setGROUP_REMOVED(notificationSettings.getInt("groupRemoved") == 1 ? "true" : "false");
+					NotificationSettingsData.setEVENTS_ADDED(notificationSettings.getInt("eventsAdded") == 1 ? "true" : "false");
+					NotificationSettingsData
+									.setEVENTS_EDITED(notificationSettings.getInt("eventsEdited") == 1 ? "true" : "false");
+					NotificationSettingsData.setEVENTS_REMOVED(notificationSettings.getInt("eventsRemoved") == 1 ? "true"
+									: "false");
+					NotificationSettingsData.setCOMMENTS_ADDED(notificationSettings.getInt("commentsAdded") == 1 ? "true"
+									: "false");
+					NotificationSettingsData.setCOMMENTS_EDITED(notificationSettings.getInt("commentsEdited") == 1 ? "true"
+									: "false");
+					NotificationSettingsData.setCOMMENTS_REMOVED(notificationSettings.getInt("commentsRemoved") == 1 ? "true"
+									: "false");
+					NotificationSettingsData.setPRIVILEGE_GIVEN(notificationSettings.getInt("privilegeGiven") == 1 ? "true"
+									: "false");
+					NotificationSettingsData.setVIBRATION(notificationSettings.getInt("vibration") == 1 ? "true" : "false");
+
 					Intent intent = new Intent(getApplicationContext(), CalendarController.class);
 					startActivity(intent);
-				}
-				catch(Exception e)
-				{
+				} catch (Exception e) {
 					e.printStackTrace();
 					logout();
 				}
 			}
 		}
 	}
-	
-	private void logout()
-	{
+
+	private void logout() {
 		resetUserData();
 		resetNotificationSettingsData();
 
@@ -235,8 +215,7 @@ public class LoginController extends Activity
 		startActivity(intent);
 	}
 
-	private void resetUserData()
-	{
+	private void resetUserData() {
 		UserData.setPERSONID("");
 		UserData.setFIRST_NAME("");
 		UserData.setLAST_NAME("");
@@ -247,8 +226,7 @@ public class LoginController extends Activity
 		UserData.setSHOWN_EVENT_ENTRIES("");
 	}
 
-	private void resetNotificationSettingsData()
-	{
+	private void resetNotificationSettingsData() {
 		NotificationSettingsData.setNOTIFICATION_SETTINGS_ID("");
 		NotificationSettingsData.setSHOW_ENTRIES("");
 		NotificationSettingsData.setGROUP_INVITES("");
