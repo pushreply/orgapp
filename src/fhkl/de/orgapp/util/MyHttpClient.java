8b58a9468234c3d77af019f4f96e289153b29dc5
ronaldo.hasiholan@gmail.com <ronaldo.hasiholan@gmail.com@15f01cda-b1fb-901b-eaea-b5b0833f664c>
2014-08-05 20:01:05 +0000

    please copy the certificate to the /res/raw/ (see email for cert)

--- src/fhkl/de/orgapp/util/MyHttpClient.java
+++ src/fhkl/de/orgapp/util/MyHttpClient.java
@@ -11,14 +11,14 @@ import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.conn.SingleClientConnManager;
 
+import fhkl.de.orgapp.R;
+
 import android.content.Context;
 
 public class MyHttpClient extends DefaultHttpClient {
 
 	final Context context;
-	final private String KEYSTORE_PW = "AndroidSS2014"; // Old:
-																											// "orgappss2014fhzw"; (do
-																											// not remove)
+	final private String KEYSTORE_PW = "AndroidSS2014";
 
 	public MyHttpClient(Context context) {
 		this.context = context;
@@ -28,8 +28,9 @@ public class MyHttpClient extends DefaultHttpClient {
 	protected ClientConnectionManager createClientConnectionManager() {
 		SchemeRegistry registry = new SchemeRegistry();
 		registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-		// Register port 443 for the SSLSocketFactory with the keystore to the
-		// ConnectionManager
+		
+		// Register port 443 for the SSLSocketFactory with 
+		// the keystore to the ConnectionManager
 		registry.register(new Scheme("https", newSslSocketFactory(), 443));
 		return new SingleClientConnManager(getParams(), registry);
 	}
@@ -39,21 +40,20 @@ public class MyHttpClient extends DefaultHttpClient {
 			// Get an instance of the Bouncy Castle Keystore format
 			KeyStore trusted = KeyStore.getInstance("BKS");
 
-			// Get the keystore from resource raw folder. The keystore is the trusted
-			// cert.
-			InputStream in = null; // =
-															// context.getResources().openRawResource(R.raw.customtruststore);
+			// Get the keystore from resource raw folder. The keystore is the
+			// trusted certificate.
+			InputStream in = context.getResources().openRawResource(R.raw.customtruststore);
 			try {
-				// Initialize keystore with the provided trusted cert and the keystore
-				// pass
+				// Initialize keystore with the provided trusted cert and 
+				// the keystore pass
 				trusted.load(in, KEYSTORE_PW.toCharArray());
 			} finally {
 				in.close();
 			}
 
-			// Pass the keystore to the SSLSocketFactory. The SSLSocketFactory is
-			// responsible for
-			// the verification of the server certificate.
+			// Pass the keystore to the SSLSocketFactory. 
+			// The SSLSocketFactory is responsible for 
+			// server certificate verification.
 			SSLSocketFactory sf = new SSLSocketFactory(trusted);
 
 			// Verify hostname of the cert.
